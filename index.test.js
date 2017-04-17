d7e43c12e3a9daa2119b3587e5d594a59595d833
ariarijp <takuya.arita@gmail.com>
2017-04-17 13:12:48 +0900

    Fix tests for asynchronous methods

--- index.test.js
+++ index.test.js
@@ -6,19 +6,19 @@ const MACKEREL_APIKEY = process.env['MACKEREL_APIKEY']
 const client = new MackerelClient({mackerelApiKey: MACKEREL_APIKEY})
 
 test('getHost', () => {
-  client.getHosts()
+  return client.getHosts()
   .then(hosts => hosts[0].id)
   .then(hostId => client.getHost(hostId))
   .then(host => expect(host).toBeTruthy())
 })
 
 test('getHosts', () => {
-  client.getHosts()
+  return client.getHosts()
   .then(hosts => expect(hosts).toBeTruthy())
 })
 
 test('getHostMetricNames', () => {
-  client.getHosts()
+  return client.getHosts()
   .then(hosts => hosts[0])
   .then(host => {
     client.getHostMetricNames(host.id)
@@ -29,7 +29,7 @@ test('getHostMetricNames', () => {
 test('getHostMetrics', () => {
   let now = moment().unix()
 
-  client.getHosts()
+  return client.getHosts()
   .then(hosts => hosts[0])
   .then(host => {
     client.getHostMetrics(host.id, 'loadavg5', now - 3600, now)
@@ -38,7 +38,7 @@ test('getHostMetrics', () => {
 })
 
 test('getLatestMetrics', () => {
-  client.getHosts()
+  return client.getHosts()
   .then(hosts => hosts.map(host => host.id))
   .then(hostIds => {
     client.getLatestMetrics(hostIds, ['loadavg5', 'cpu.user.percentage', 'memory.used'])
